8380ae04c7f13849b038529356015ccfcd5c1547
Shahriat Hossain <somel90@gmail.com>
2022-04-19 21:51:09 +0600

    backend code updated

--- lumen_v1/questionnaire/app/Http/Controllers/SurveyAnswerController.php
+++ lumen_v1/questionnaire/app/Http/Controllers/SurveyAnswerController.php
@@ -21,7 +21,18 @@ class SurveyAnswerController extends Controller
     // FIND
     public function index()
     {
-        return response()->json(SurveyAnswer::all());
+        $results = app('db')->select(
+            "(SELECT s.name AS Survey, q.title AS Question, c.label AS Chocie, COUNT(c.id) AS Responses, 
+        a.answer_text AS Text FROM questions AS q, multiple_choices AS c, survey_answers AS a, surveys AS s 
+        WHERE a.question_id = q.id AND q.survey_id=s.id AND a.multiple_choice_id = c.id 
+        GROUP BY a.multiple_choice_id) 
+        UNION ALL 
+        (SELECT s.name AS Survey, q.title AS Question, c.label AS Chocie, COUNT(c.id) AS Responses, 
+        a.answer_text AS Text FROM questions AS q, multiple_choices AS c, survey_answers AS a, 
+        surveys AS s WHERE a.question_id = q.id AND q.survey_id=s.id AND a.multiple_choice_id IS NULL
+        GROUP BY a.multiple_choice_id)"
+        );
+        return response()->json($results);
     }
 
     public function show($id)
